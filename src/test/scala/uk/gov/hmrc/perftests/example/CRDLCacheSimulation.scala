0efc322fd62517d45e322d5d80d43a7e379ca5b5
David Gregory <2992938+DavidGregory084@users.noreply.github.com>
2025-07-16 17:07:42 +0100

    CRDL-322 Implement performance tests for the crdl-cache service

---
+++ src/test/scala/uk/gov/hmrc/perftests/example/CRDLCacheSimulation.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.example
+
+import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
+import uk.gov.hmrc.perftests.example.CRDLCacheRequests._
+import uk.gov.hmrc.performance.conf.ServicesConfiguration
+
+class CRDLCacheSimulation extends PerformanceTestRunner with ServicesConfiguration {
+  val crdlCacheUrl   = s"${baseUrlFor("crdl-cache")}/crdl-cache"
+  val emcsRefDataUrl = s"${baseUrlFor("emcs-tfe-crdl-reference-data")}/emcs-tfe-reference-data"
+
+  def deleteCrdlData(entity: String) = {
+    requests.delete(s"$crdlCacheUrl/test-only/$entity")
+  }
+  
+  def importCrdlCata(entity: String) = {
+    requests.post(s"$crdlCacheUrl/test-only/$entity")
+  }
+
+  def getCrdlImportStatus(entity: String) = {
+    val response = requests.get(s"$crdlCacheUrl/test-only/$entity")
+    val json     = ujson.read(response)
+    json("status").str
+  }
+
+  before {
+    // Clear down any existing data
+    deleteCrdlData("last-updated")
+    deleteCrdlData("codelists")
+    deleteCrdlData("correspondence-lists")
+    // Import everything again
+    importCrdlCata("codelists")
+    importCrdlCata("correspondence-lists")
+    // Wait for the imports to complete
+    while (getCrdlImportStatus("codelists") != "IDLE") {
+      Thread.sleep(200)
+    }
+    while (getCrdlImportStatus("correspondence-lists") != "IDLE") {
+      Thread.sleep(200)
+    }
+  }
+
+  setup("create-draft-movement", "Create A Draft Movement").withRequests(
+    fetchCountries,
+    fetchMemberStates,
+    fetchPackagingTypes,
+    fetchTransportUnits,
+    fetchWineOperations,
+    fetchDocumentTypes
+  )
+
+  runSimulation()
+}
